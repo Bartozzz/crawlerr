68df080dacf19e2342eeb11a97aea453d9672179
Łaniewski Bartosz <laniewski.bartozzz@gmail.com>
2017-08-30 16:20:02 +0200

    Update examples

--- examples/request_title.js
+++ examples/request_title.js
@@ -2,7 +2,7 @@ const crawler = require( "../dist" );
 const spider  = crawler( "http://google.com" );
 
 spider.request( "/", ( req, res ) => {
-    console.log( res.get( "title" ).html() )
+    console.log( res.get( "title" ).html() );
 }, error => {
     console.log( error );
 } );

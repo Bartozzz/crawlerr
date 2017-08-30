87a28a1e8466e46129a746691763a2e2642e3fa8
Łaniewski Bartosz <laniewski.bartozzz@gmail.com>
2017-08-30 18:17:26 +0200

    Add get method to request a single resource

--- src/routing/router.js
+++ src/routing/router.js
@@ -1,4 +1,5 @@
 import url      from "url";
+import request  from "retry-request";
 import mixin    from "merge-descriptors";
 import wildcard from "wildcard-named";
 
@@ -11,6 +12,32 @@ export default {
         } );
     },
 
+    get( uri ) {
+        if ( !uri.startsWith( this.base ) ) {
+            uri = url.resolve( this.base, uri );
+        }
+
+        return new Promise( ( resolve, reject ) => {
+            request( uri, ( error, response ) => {
+                if ( error || response.statusCode != 200 ) {
+                    reject( error || uri );
+                }
+
+                const req = {};
+                const res = response;
+
+                // Set circular references:
+                res.req = req;
+                req.res = res;
+
+                // Alter the prototypes:
+                req.__proto__ = this.req;
+                res.__proto__ = this.res;
+
+                resolve( { req, res, uri } );
+            } );
+        } );
+    },
     check( uri, req, res ) {
         for ( const index in this.callbacks ) {
             const requested = wildcard( uri, index );

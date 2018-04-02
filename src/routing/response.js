44ae40964db1f5c45d0aa4a66901ae1e7d5b7ee7
Bartosz Łaniewski <laniewski.bartozzz@gmail.com>
2018-04-03 00:28:49 +0200

    Add Flow types in response

--- src/routing/response.js
+++ src/routing/response.js
@@ -1,15 +1,15 @@
 import { JSDOM } from "jsdom";
 
 export default {
-  get jsdom() {
+  get jsdom(): JSDOM {
     return new JSDOM(this.body);
   },
 
-  get window() {
+  get window(): JSDOM {
     return this.jsdom.window;
   },
 
-  get document() {
+  get document(): JSDOM {
     return this.window.document;
   }
 };

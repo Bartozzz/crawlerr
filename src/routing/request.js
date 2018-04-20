ca1ab3429924652d956ea8b98b2c31d62680ef8f
Bartosz Łaniewski <laniewski.bartozzz@gmail.com>
2018-04-20 12:01:22 +0200

    Fix "Missing type annotation" in Flow 0.70.0

--- src/routing/request.js
+++ src/routing/request.js
@@ -3,66 +3,68 @@
 import http from "http";
 import typeis from "type-is";
 
-export default Object.create(http.IncomingMessage.prototype, {
-  /**
-   * Returns request header. The `Referrer` header field is special-cased,
-   * both `Referrer` and `Referer` are interchangeable.
-   *
-   * @param   {string}    header
-   * @return  {string}
-   * @access  public
-   */
-  get(header: string): string {
-    if (!header || typeof header !== "string") {
-      throw new TypeError(
-        `req.get takes a string as argument, got ${typeof header}`
-      );
-    }
+const req = Object.create(http.IncomingMessage.prototype);
 
-    const lower: string = header.toLowerCase();
-
-    switch (lower) {
-      case "referer":
-      case "referrer":
-        return this.headers.referrer || this.headers.referer;
-
-      default:
-        return this.headers[lower];
-    }
-  },
-
-  /**
-   * Check if the incoming request contains the "Content-Type" header field,
-   * and it contains the give mime `type`.
-   *
-   * @param   {string|Array}      types...
-   * @return  {string|false}
-   * @access  public
-   */
-  is(...types: Array<string>): string | boolean {
-    return typeis(this, types);
-  },
-
-  /**
-   * Return the value of param `name` when present or `defaultValue`:
-   * - checks route placeholders, ex: `user/[all:username]`;
-   * - checks body params, ex: `id=12, {"id":12}`;
-   * - checks query string params, ex: `?id=12`;
-   *
-   * @param   {string}    name
-   * @param   {any}       defaultValue
-   * @return  {string}
-   * @access  public
-   */
-  param(name: string, defaultValue: any): any {
-    const params: Object = this.params || {};
-    const query: Object = this.query || {};
-    const body: Object = this.body || {};
-
-    if (params[name] != null) return params[name];
-    if (query[name] != null) return query[name];
-    if (body[name] != null) return body[name];
-
-    return defaultValue;
+/**
+ * Returns request header. The `Referrer` header field is special-cased, both
+ * `Referrer` and `Referer` are interchangeable.
+ *
+ * @param   {string}    header
+ * @return  {string}
+ * @access  public
+ */
+req.get = function(header: string): string {
+  if (!header || typeof header !== "string") {
+    throw new TypeError(
+      `req.get takes a string as argument, got ${typeof header}`
+    );
   }
-});
+
+  const lower: string = header.toLowerCase();
+
+  switch (lower) {
+    case "referer":
+    case "referrer":
+      return this.headers.referrer || this.headers.referer;
+
+    default:
+      return this.headers[lower];
+  }
+};
+
+/**
+ * Check if the incoming request contains the "Content-Type" header field and it
+ * contains the give mime `type`.
+ *
+ * @param   {string|Array}      types...
+ * @return  {string|false}
+ * @access  public
+ */
+req.is = function(...types: Array<string>): string | boolean {
+  return typeis(this, types);
+};
+
+/**
+ * Return the value of param `name` when present or `defaultValue`:
+ * - checks route placeholders, ex: `user/[all:username]`;
+ * - checks body params, ex: `id=12, {"id":12}`;
+ * - checks query string params, ex: `?id=12`;
+ *
+ * @param   {string}    name
+ * @param   {any}       defaultValue
+ * @return  {string}
+ * @access  public
+ */
+req.param = function(name: string, defaultValue: any): any {
+  const params: Object = this.params || {};
+  const query: Object = this.query || {};
+  const body: Object = this.body || {};
+
+  if (params[name] != null) return params[name];
+  if (query[name] != null) return query[name];
+  if (body[name] != null) return body[name];
+
+  return defaultValue;
+};
+
+export default req;
